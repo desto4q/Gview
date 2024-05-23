07721b4a22851f3bc9885c111549a3f90298c1b9
desto4q <desto4q@gmail.com>
2024-05-24 00:18:55 +0100

    pre remake

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import Main from './app/Main';
 import crypto from "crypto"
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { vexo } from 'vexo-analytics';
-if (!__DEV__) { vexo("d9c9b63c-80dd-41b4-84d6-8325a62de15b") }
+// if (!__DEV__) { vexo("d9c9b63c-80dd-41b4-84d6-8325a62de15b") }
 
 
 export default function App() {
